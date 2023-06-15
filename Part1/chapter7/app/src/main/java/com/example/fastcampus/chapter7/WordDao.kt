7f57e36e94f11fbffd662eb071bea0023201e7c3
unso99 <94777814+unso99@users.noreply.github.com>
2023-06-15 21:19:36 +0900

    Add files via upload

---
+++ Part1/chapter7/app/src/main/java/com/example/fastcampus/chapter7/WordDao.kt
@@ -0,0 +1,25 @@
+package com.example.fastcampus.chapter7
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface WordDao {
+    @Query("SELECT * from word ORDER BY id DESC")
+    fun getAll():List<Word>
+
+    @Query("SELECT * from word ORDER BY id DESC LIMIT 1")
+    fun getLatestWord() :Word
+
+    @Insert
+    fun insert(word:Word)
+
+    @Delete
+    fun delete(word:Word)
+
+    @Update
+    fun update(word: Word)
+}
