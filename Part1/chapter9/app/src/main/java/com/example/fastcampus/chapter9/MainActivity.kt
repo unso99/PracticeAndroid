771f3d7942a5124b30649f9e9ce9814220357937
unso99 <94777814+unso99@users.noreply.github.com>
2023-06-20 21:36:07 +0900

    Add files via upload

---
+++ Part1/chapter9/app/src/main/java/com/example/fastcampus/chapter9/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.fastcampus.chapter9
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fastcampus.chapter9.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.playButton.setOnClickListener { mediaPlayerPlay() }
+        binding.pauseButton.setOnClickListener { mediaPlayerPause() }
+        binding.stopButton.setOnClickListener { mediaPlayerStop() }
+    }
+
+    private fun mediaPlayerPlay() {
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply { action = MEDIA_PLAYER_PLAY }
+        startService(intent)
+    }
+
+    private fun mediaPlayerPause() {
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply { action = MEDIA_PLAYER_PAUSE }
+        startService(intent)
+    }
+
+    private fun mediaPlayerStop() {
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply { action = MEDIA_PLAYER_STOP }
+        startService(intent)
+    }
+
+    override fun onDestroy() {
+        stopService(Intent(this, MediaPlayerService::class.java))
+        super.onDestroy()
+    }
+}
